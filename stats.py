fdbfc1ed27c88c0142aca454b4090d69602043d9
rolexx <aregmi29@gmail.com>
2022-12-14 10:34:05 -0500

    commit

--- stats.py
+++ stats.py
@@ -11,38 +11,46 @@ html = requests.get(url=url, headers=url_header).text
 soup = BeautifulSoup(html, "html.parser")
 
 stats_url = []
-
 soup = soup.find_all("ul")[3].find_all('li', recursive=False)[3].find_all("li")
-
 for l in soup:
     stats_url.append(base_url+l.find("a")['href'])
 
-url = stats_url[0]
+def get_Data(url, url_header=None):
 
-table_columns = []
-    
-html = requests.get(url=url, headers=url_header).text
-soup = BeautifulSoup(html, "html.parser")
-table = soup.find("table")
-
-table_header = table.find("thead").find_all('tr')[1]
-for h in table_header.find_all("th")[1:]:
-    table_columns.append(h.text.strip())
-
-statsFrame = pd.DataFrame(columns=table_columns)
-
-table_body = table.find("tbody")
-for row in table_body.find_all("tr"):
-    try:
-        if row['class']:
-            continue
-    except:
-        temp = []
-        cols = row.find_all("td")
+    table_columns = []
         
-        for col in cols:
-            temp.append(col.text)
-        statsFrame = pd.concat([statsFrame, pd.DataFrame([temp], columns=statsFrame.columns)], ignore_index=True)
-            
+    html = requests.get(url=url, headers=url_header).text
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find("table")
 
-print(statsFrame)
+    table_header = table.find("thead").find_all('tr')[1]
+    for h in table_header.find_all("th")[1:]:
+        table_columns.append(h.text.strip())
+
+    statsFrame = pd.DataFrame(columns=table_columns)
+
+    table_body = table.find("tbody")
+    for row in table_body.find_all("tr"):
+        try:
+            if row['class']:
+                continue
+        except:
+            temp = []
+            cols = row.find_all("td")
+            
+            for col in cols:
+                temp.append(col.text)
+            statsFrame = pd.concat([statsFrame, pd.DataFrame([temp], columns=statsFrame.columns)], ignore_index=True)
+                
+    return statsFrame
+    
+    
+initialFrame = get_Data(stats_url[0])
+
+for url in stats_url[3:4]:
+    initialFrame = initialFrame.merge(get_Data(url), on=["Player"], copy=False, how="outer", suffixes=("_DRIP", "_DROP"))
+    
+print(initialFrame)
+
+print(initialFrame.columns)
+print(initialFrame.iloc[0])
